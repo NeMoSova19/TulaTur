bde8bc9a485b5f9c269b92e264b4e86d0513340b
whoshog <bason11111@gmail.com>
2024-04-08 23:54:11 +0300

    Сделал верству болванки для будущих страниц мест.
    Использованные картинки добавил в двух расцветках (белый и черный).

--- blank.php
+++ blank.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,22 +43,35 @@
         </section>
         <section class="section_background"></section>
         <section class="main_app_background">
-            <div class="discription">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatum autem exercitationem, qui non pariatur.</p>
-            </div>
-            <div class="adress">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-            </div>
-            <div class="like-dislike">
-                <img class="icon_like" src="img/like_black.png" alt="Like">
-                <img class="icon_dislike" src="img/dislike_black.png" alt="Dislike">
-            </div>
-            <div class="comments">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos qui officia doloribus doloremque sunt et enim dignissimos, quam ipsum non!</p>
-            </div>
-            <div class="add_to_visited">
-                <img  class="icon_add_to_visited" src="img/icon_suitcase.png" alt="Add to visited">
-            </div>
+            <section class="place_info">
+                <div class="discription">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatum autem exercitationem, qui non pariatur.</p>
+                </div>
+                <div class="adress">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+                </div>
+                <div class="buttons">
+                    <button class="btn btn_like" title="Нравится"><img class="icon_like" src="img/like_black.png" alt="Like"></button>
+                    <button class="btn btn_dislike" title="Не нравится"><img class="icon_dislike" src="img/dislike_black.png" alt="Dislike"></button>
+                    <button class="btn btn_add_to_visited" title="Добавить в избранное"><img class="icon_add_to_visited" src="img/icon_suitcase.png" alt="Add to visited"></button>
+                    <button class="btn btn_planet_link" title="Перейти на сайт"><img class="icon_planet_link" src="img/planet_link_black.png" alt="Visit web-site"></button>
+                </div>
+                <div class="map">
+                    <p>а я карта я карта</p>
+                    <p>я рюкзак я рюкзак</p>
+                </div>
+                <div class="comments">
+                    <p>Комменты:</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                    <p>Политический срач в комментах</p>
+                </div>
+            </section>
         </section>
 </body>
 </html>
