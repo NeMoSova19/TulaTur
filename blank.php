ccfa9509eb2e831bad8ade109d6e50cda5e0b9de
NeMoSova19 <ya.ffrg20162016@yandex.ru>
2024-04-18 15:37:16 +0300

    ...

--- blank.php
+++ blank.php
@@ -52,24 +52,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$place["Name"]?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/header.css" type="text/css">
+    
     <link rel="stylesheet" href="css/blank.css">
     <link rel="stylesheet" href="css/adaptive_blank.css"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet">
     <script src="https://api-maps.yandex.ru/2.1/?apikey=f697c4a8-f8c8-478d-a897-005e1cc67a13&load=package.standard&lang=ru-RU" type="text/javascript"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="js/header.js"></script>
+    
+    
     <script src="js/map.js"></script>
     <script src="js/like_dislike.js"></script>
     <script src="js/favorites.js"></script>
     <script src="js/comments.js"></script>
+
 </head>
+<?php include "header.php"; ?>
 <body>
-    <?php include "header.php"; ?>
 
     <main>
         <section class="section_main">
--- visited.php
+++ visited.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    $_SESSION['prev_page'] = "visited.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,25 +10,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/header.css" type="text/css">
     <link rel="stylesheet" href="css/liked_and_visited.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="js/header.js"></script>
 </head>
+<?php include "header.php"; ?>
 <body>
-    <?php include "header.php"; ?>
 
     <main>
         <section class="section_main">
             <div class="main_title-block">
-                <div class="title"><h1 class="main_title">Мои поездки</h1></div>
                 <img class="main_background-img" src="img/tula_day.jpg" alt="">
                 <img class="main_background-img img-night" src="img/tula_night.jpg" alt="" style="display: none;">
             </div>
         </section>
+        <div class="title"><h1 class="main_title">Мои поездки</h1></div>
         <section class="section_background"></section>
         <section class="main_app_background">
             <div class="card">
